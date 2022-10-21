6dc8c47208cfd6a08c9763b33dcf264c96a3ff7e
Brian Liao <yaramiu@gmail.com>
2022-10-20 21:43:15 -0400

    Let players mark specific spots on the board

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,40 +1,78 @@
-const player = (name, symbol, isFirst) => {
-  return { name, symbol, isFirst };
+const player = (name, marker, isFirst) => {
+  return { name, marker, isFirst };
 };
 
 const gameBoard = (() => {
-  let gameboard = [];
+  let gameboard = ["", "", "", "", "", "", "", "", ""];
 
-  return { gameboard };
+  const reset = () => {
+    gameboard = ["", "", "", "", "", "", "", "", ""];
+  };
+
+  const get = () => {
+    return gameboard;
+  };
+
+  const change = (index, marker) => {
+    gameboard[index] = marker;
+  };
+
+  const isIndexEmpty = (index) => {
+    if (gameboard[index] === "") {
+      return true;
+    }
+    return false;
+  };
+
+  return { get, reset, change, isIndexEmpty };
 })();
 
 const displayController = (() => {
-  let gameboardGrid = document.querySelector(".gameboard-grid");
-  let gameboardCells = gameboardGrid.children;
+  const gameboardGrid = document.querySelector(".gameboard-grid");
+  const gameboardCells = gameboardGrid.children;
+  let turn = 1;
+  const MAX_TURNS = 9;
 
-  const changeGameboardDisplay = (change) => {
+  const showGameboardDisplay = () => {
     for (let index = 0; index < gameboardCells.length; index++) {
       const gameboardCell = gameboardCells[index];
-      const gameboardCellContent = gameboardCell.firstChild;
+      const gameboardCellContent = gameboardCell.children[0];
 
-      if (change === "show") {
-        gameboardCellContent.textContent = gameBoard.gameboard[index];
-      } else if (change === "clear") {
-        gameboardCellContent.textContent = "";
-      }
+      gameboardCellContent.textContent = gameBoard.get()[index];
     }
   };
 
-  const showGameboardDisplay = () => {
-    changeGameboardDisplay("show");
-  };
-
   const clearGameboardDisplay = () => {
-    changeGameboardDisplay("clear");
+    gameBoard.reset();
+    showGameboardDisplay();
   };
 
-  return { showGameboardDisplay, clearGameboardDisplay };
+  const ticTacToe = () => {
+    for (let index = 0; index < gameboardCells.length; index++) {
+      const gameboardCell = gameboardCells[index];
+      const gameboardCellContent = gameboardCell.children[0];
+
+      gameboardCellContent.addEventListener("click", () => {
+        if (turn <= MAX_TURNS) {
+          if (gameBoard.isIndexEmpty(index)) {
+            if (turn % 2 !== 0) {
+              gameBoard.change(index, "X");
+            } else {
+              gameBoard.change(index, "O");
+            }
+            turn++;
+            showGameboardDisplay();
+          }
+        }
+      });
+    }
+  };
+
+  return {
+    showGameboardDisplay,
+    clearGameboardDisplay,
+    ticTacToe,
+  };
 })();
 
-gameBoard.gameboard = ["X", "X", "O", "O", "X", "O", "O", "X", "O"];
-displayController.showGameboardDisplay();
+displayController.ticTacToe();
